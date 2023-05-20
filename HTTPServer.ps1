1b11aab701b9dc7945be463402b51948128bb3c5
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2023-05-20 23:18:26 +0300

    Update HTTPServer.ps1

--- HTTPServer.ps1
+++ HTTPServer.ps1
@@ -1,40 +1,12 @@
-﻿function Send-ResponseString ($ResponseObject, $StatusCode, $ContentType, $String) {
+﻿Import-Module -Name ".\HTTP.psm1"
+Import-Module -Name ".\Variables.ps1"
 
-    # Set the response status code and content type
-    $ResponseObject.StatusCode = 200
-    $ResponseObject.ContentType = "text/plain"
-
-    # Write the response body
-    $buffer = [System.Text.Encoding]::UTF8.GetBytes($String)
-    $ResponseObject.ContentLength64 = $buffer.Length
-    $ResponseObject.OutputStream.Write($buffer, 0, $buffer.Length)
-
-    # Close the response to send it to the client
-    $ResponseObject.Close()
-}
-
-function Send-ResponseFile ($ResponseObject, $StatusCode, $ContentType, $Path) {
-
-    # Set the response status code and content type
-    $response.StatusCode = 200
-    $response.ContentType = "application/octet-stream"
-
-    # Read the file and write it to the response body
-    $buffer = [System.IO.File]::ReadAllBytes($Path)
-    $response.ContentLength64 = $buffer.Length
-    $response.OutputStream.Write($buffer, 0, $buffer.Length)
-
-    # Close the response to send it to the client
-    $response.Close()
-}
-
-$VerbosePreference="Continue"
-Clear-Host
-$Continue=$true
-$Listener=[System.Net.HttpListener]::new()
-$Listener.Prefixes.Add("http://+:8080/")
+$continue=$true
+$Listener = [System.Net.HttpListener]::new()
+$Listener.Prefixes.Add($URL)
 $Listener.Start()
-while ($Continue) {
+
+while ($continue) {
   $context = $Listener.GetContext()
   $request = $context.Request
   $response = $context.Response
@@ -67,7 +39,7 @@ while ($Continue) {
         }
     }
 
-    $Continue = $false
+    $continue = $false
   }
 }
 $Listener.Stop()
